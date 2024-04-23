5eabffc0299acb24418cfd504dae2c06bfae9f6c
zichenmei <143123810+zichenmei@users.noreply.github.com>
2024-04-22 20:30:22 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -2,7 +2,22 @@ import java.util.*;
 public class RandomStringChooser
 {
   /* to be implemented in part (a) */
-  
+  private String[] wordList;
+  private int ind;
+
+  public RandomStringChooser(String[] list){
+    wordList=list;
+    ind=0;
+  }
+  public String getNext(){
+    if(ind<word){
+      String word=wordList[ind];
+      ind++;
+      return word;
+    }else{
+      return "NONE";
+    }
+  }
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
 }
